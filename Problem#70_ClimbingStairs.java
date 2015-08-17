b2e94a3aae1348f0cce950e5b541540c9d93035b
Ke@CMU <keer236@gmail.com>
2015-08-16 23:01:22 -0400

    Nothing much

---
+++ Problem#70_ClimbingStairs.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        int ans[] = new int[n];
+        ans[0] = 1;
+        ans[1] = 2;
+        for (int i = 2; i < n; i++)
+            ans[i] = ans[i-1] + ans[i-2];
+        return ans[n-1];
+    }
+}
