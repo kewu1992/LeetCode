b1f154813678ab89d6e1fd861e88439a9a42bf28
Ke@CMU <keer236@gmail.com>
2015-08-22 18:58:51 -0400

    * Easy ignore corner cases

---
+++ Problem#102_BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    class Element{
+        TreeNode root;
+        int level;
+        Element(TreeNode r, int l){
+            root = r;
+            level = l;
+        }
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<Element> queue = new LinkedList();
+        List<List<Integer>> ans = new LinkedList();
+        List<Integer> now = new LinkedList();
+        if (root == null)
+            return ans;
+        queue.add(new Element(root, 1));
+        int nowLevel = 1;
+        while (!queue.isEmpty()){
+            Element temp = queue.remove();
+            if (nowLevel != temp.level){
+                ans.add(now);
+                now = new LinkedList();
+                nowLevel = temp.level;
+            }
+            now.add(temp.root.val);
+            if (temp.root.left != null)
+                queue.add(new Element(temp.root.left, temp.level + 1));
+            if (temp.root.right != null)
+                queue.add(new Element(temp.root.right, temp.level + 1));
+        }
+        ans.add(now);
+        return ans;
+    }
+}
