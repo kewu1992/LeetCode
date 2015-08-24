7c660f946f0268f5bda620f728d98e2834ce6398
Ke@CMU <keer236@gmail.com>
2015-08-24 15:38:52 -0400

    Nothing much

---
+++ Problem#126_SingleNumber.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            ans = ans ^ nums[i];
+        return ans;
+    }
+}
