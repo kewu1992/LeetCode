e3c42112c2f22d4d63ac997405ca3a30c8052d36
Ke@CMU <keer236@gmail.com>
2015-09-03 12:12:27 -0400

    * a kind of binary search

---
+++ Problem#162_FindPeakElement.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 1)
+            return 0;
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi){
+            int mid = (lo + hi) / 2;
+            if (mid == 0) {
+                if (nums[mid] > nums[mid+1])
+                    return mid;
+                else
+                    lo = mid + 1;
+            }
+            else if (mid == nums.length-1){
+                if (nums[mid] > nums[mid-1])
+                    return mid;
+                else
+                    hi = mid - 1;
+            } else{
+                if (nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1])
+                    return mid;
+                else if (nums[mid-1] < nums[mid] && nums[mid] < nums[mid+1])
+                    lo = mid + 1;
+                else
+                    hi = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
